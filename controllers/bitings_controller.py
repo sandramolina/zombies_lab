64708bfa63c1afd4d08fdf83a2a26ab7a9b36212
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-04-21 16:49:49 +0100

    WIP on the add new biting function

--- controllers/bitings_controller.py
+++ controllers/bitings_controller.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, Flask, redirect, render_template, request
+from models.biting import Biting
 from models.zombie import Zombie
 import repositories.biting_repository as biting_repository
 import repositories.zombie_repository as zombie_repository
+import repositories.human_repository as human_repository
 
 bitings_blueprint = Blueprint("bitings", __name__)
 
@@ -11,18 +13,21 @@ def bitings():
     bitings = biting_repository.select_all()
     return render_template('bitings/index.html', bitings = bitings)
 
+# NEW
+@bitings_blueprint.route("/bitings/new")
+def add_new_biting():
+    zombies = zombie_repository.select_all()
+    humans = human_repository.select_all()
+    return render_template('bitings/new.html', humans = humans, zombies = zombies)
+
 # CREATE
 @bitings_blueprint.route("/bitings", methods=["POST"])
 def create_biting():
     human = request.form['human_id']
     zombie = request.form["zombie_id"]
-
-def create_zombie_type():
-    name = request.form["name"]
-    new_zombie_type = ZombieType(name)
-    zombie_type_repository.save(new_zombie_type)
-    return redirect("/zombietypes")
-
+    new_biting = Biting(human, zombie)
+    biting_repository.save(new_biting)
+    return redirect('/bitings')
 
 # EDIT
 
