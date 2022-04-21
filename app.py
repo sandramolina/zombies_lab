b53853ae0d9d10192e512f7a028f17e041aa6c5f
Sandra Molina Palencia <smolinapalencia@gmail.com>
2022-04-21 15:02:56 +0100

    adds bp to app

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from flask import Flask, render_template
 from controllers.humans_controller import humans_blueprint
 from controllers.zombies_controller import zombies_blueprint
 from controllers.zombie_types_controller import zombie_types_blueprint
+from controllers.bitings_controller import bitings_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(humans_blueprint)
 app.register_blueprint(zombies_blueprint)
 app.register_blueprint(zombie_types_blueprint)
+app.register_blueprint(bitings_blueprint)
 
 @app.route("/")
 def main():
